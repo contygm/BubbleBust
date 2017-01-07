ea992205edbbf4860297c772125e5a63d7cf25a5
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2017-01-07 16:09:21 -0500

    trying to pull specific pieces

--- server.js
+++ server.js
@@ -6,11 +6,6 @@ var mongoose = require("mongoose");
 var Promise = require("bluebird");
 mongoose.Promise = Promise;
 
-var twitterAPI = require('node-twitter-api');
-var twitter = new twitterAPI({
-    consumerKey: 'DET3lqXYoKyT9jAebSHQFmgJ8',
-    consumerSecret: 'FtEuum2jzoXlEZQT8dNn2jJxC8jiYdC1AQm3gsxiV0GiMkMZKF'
-});
 
 // Require Schemas
 var Executive = require("./models/Executive");
@@ -57,6 +52,16 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+var Twitter = require("twitter");
+
+twitterKeys = {
+  consumer_key: "DET3lqXYoKyT9jAebSHQFmgJ8",
+  consumer_secret: "FtEuum2jzoXlEZQT8dNn2jJxC8jiYdC1AQm3gsxiV0GiMkMZKF",
+  access_token_key: "63023948-1em4lPI07V8hh69NTOrbT4nG9eJwjePbf2dFt13Jd",
+  access_token_secret: "ZgMuaTKCbxjVQLIewzdyPnQ4CwJBCvYhCW2t92mLBo9Vb"
+};
+
+var client = new Twitter(twitterKeys);
 
 
 // TODO: switch case when it's working
@@ -75,7 +80,18 @@ app.get("/twitter/:collection", function(req, res) {
 	      console.log(err);
 	    }
 	    else {
-	      res.send(doc);
+	    	for (var n = 0; n < doc.length; n++){
+	    		if (doc[n].handle != "n/a"){
+	    			var params = { screen_name: doc[n].handle };
+					console.log(doc[n].handle);
+					
+					client.get("statuses/user_timeline", params, function(error, tweets, response) {
+					    if (!error) {
+					      res.send(response.name +" a.k.a "+ response.screen_name +"<br>"+ response.text);
+					    }
+					});
+	    		}			
+			}
 	    }
 	})
 
